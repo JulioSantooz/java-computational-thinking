1cad1535b9b37dc9eb55e6b33702c1d44d54bee2
Julio Santos <juliodiaz_santos@hotmail.com>
2023-12-10 20:41:39 -0300

    aula 2

---
+++ src/aula2/DesvioEncadeadoJogo.java
@@ -0,0 +1,33 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class DesvioEncadeadoJogo {
+    public static void main(String[] args) {
+
+        /*Uma ONG criou um jogo para alertar as pessoas sobre os riscos das drogas.
+         *Por se tratar de um assunto sensível, ficou decidido que a idade mínima para jogar é de 12 anos.
+         *Sua função é criar um programa que receba a idade do usuário e exiba a mensagem "Você pode jogar"
+         * caso ele tenha 12 anos ou mais. */
+
+        Scanner leitor = new Scanner(System.in);
+        int idade;
+
+        // Entrada
+        System.out.println("Por favor, digite a sua idade: ");
+        idade = leitor.nextInt();
+
+        System.out.println("Deixe-me verificar sua idade...");
+
+        // Processamento e saida
+        if(idade < 12) {
+            System.out.println("Você não pode jogar, pois é menor de 12 anos");
+        } else if(idade >= 18) {
+            System.out.println("Você pode jogar");
+        } else {
+            System.out.println("Você pode jogar com autorização do responsável.");
+        }
+
+        leitor.close();
+    }
+}
